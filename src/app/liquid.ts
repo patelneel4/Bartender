e6b7e6f78371a0c977f9bc529cc6f6f6c8545fa4
millerm1993 <mmiller1993@hotmail.com>
2019-09-28 03:41:43 +0100

    Adding pump to liquid page

--- src/app/liquid.ts
+++ src/app/liquid.ts
@@ -2,4 +2,5 @@ export class Liquid {
     id: number;
     name: string;
     brand: string;
+    pump: string;
 }
--- src/app/liquids/liquids.component.ts
+++ src/app/liquids/liquids.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import { Liquid } from '../liquid';
 import { LiquidService } from '../liquid.service';
+import { Pump } from '../pump';
+import { PumpsService } from '../pumps.service';
 
 @Component({
   selector: 'app-liquids',
@@ -10,16 +12,27 @@ import { LiquidService } from '../liquid.service';
 })
 export class LiquidsComponent implements OnInit {
   liquids: Liquid[];
+  pumps: Pump[];
 
-  constructor(private liquidService: LiquidService, private modalService: NgbModal) { }
+  constructor(private pumpService: PumpsService, private liquidService: LiquidService, private modalService: NgbModal) { }
 
   ngOnInit() {
     this.getLiquids();
+    this.getPumps();
   }
 
   getLiquids(): void {
     this.liquidService.getLiquids()
     .subscribe(liquids => this.liquids = liquids);
+    for (let l of this.liquids) {
+      let pump = this.pumps.find(({ liquid }) => liquid === l.id);
+      l.pump = pump.name;
+    };
+  }
+
+  getPumps(): void {
+    this.pumpService.getPumps()
+    .subscribe(pumps => this.pumps = pumps);
   }
 
   add(name: string, brand: string): void {
